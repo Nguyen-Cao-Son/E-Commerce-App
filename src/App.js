07bc736e9266b2a191f9a5de46ca106b93a66660
Son Nguyen <nguyencaoson8112k@gmail.com>
2022-07-18 12:47:10 +0700

    product-item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
+console.log(Grid)
 const App = () => {
   return (
     <div>
+  
         son nguyen
     </div>
   )
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import  Grid  from "@material-ui/core"; 
 
 const products = [
-{ id:1,name:'shoe',description:'shoe running'},
-{ id:2,name:'Iphone',description:'Iphone 8 plus '}
+{ id:1,name:'shoe',description:'shoe running',price:'$5'},
+{ id:2,name:'Iphone',description:'Iphone 8 plus ',price:'$18'}
 
 ]
 
@@ -16,8 +16,8 @@ const Products = () =>{
     //    <div>product.id</div>   
     //    <div>product.name</div>   
     //    <div>product.description</div>   
-      <Grid item key={product.id} xs={12} sm=6{} md={4} lg={3}>
-         <Product />
+      <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+         <Product product={product} />
        </Grid>
 
       )
