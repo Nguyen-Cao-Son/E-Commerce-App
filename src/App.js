91e6f8838255b4d8a27f2b31c5768f4a59729f6e
Son Nguyen <nguyencaoson8112k@gmail.com>
2022-07-19 09:47:28 +0700

    call api form https://commercejs.com

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
+import { commerce } from './lib/commerce'
 
 // import Products from './Components/products/Products.jsx'
 // import Navbar from './Components/Navbar/Navbar.jsx'
 import{ Products,Navbar} from './Components'
 
 const App = () => {
+  const [products,setProducts] = useState([
+    
+  ])
+  const fetchProducts = async () =>{
+      const {data} = await commerce.products.list();
+      setProducts(data)
+  }
+  useEffect(()=>{
+    fetchProducts();
+
+  },[])
+  console.log(products)
   return (
     <div>
        <Navbar />
-       <Products />
+       <Products products={products} />
        
     </div>
   )
 }
-
+ 
 export default App
